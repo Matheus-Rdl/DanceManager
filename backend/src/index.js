1333af6e7c824bf9d3e345fa39c419ad4d15119b
Matheus <matheus_rdl@outlook.com>
2026-08-27 13:01:55 -0300

    fix: configure cors and render port

--- backend/src/index.js
+++ backend/src/index.js
@@ -40,7 +40,7 @@ async function main() {
     origin: [
       "http://localhost:5173",
       "http://localhost:5174",
-      "https://dance-manager-gamma.vercel.app/"
+      "https://dance-manager-gamma.vercel.app"
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
